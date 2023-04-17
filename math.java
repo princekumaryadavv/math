04ce7118272486689ab66b2e03f59c6c859507a7
princekumaryadavv <129711840+princekumaryadavv@users.noreply.github.com>
2023-04-17 12:09:14 +0530

    math

---
+++ math.java
@@ -0,0 +1,13 @@
+package javajhij;
+
+public class math {
+
+	public static void main(String[] args) {
+		double x;
+		x = Math.max(22,33);
+		System.out.println("The maximum for among 22 and 33 is " +x);
+        x=Math.sqrt(64);
+        System.out.println("The square root of this number is "+x);
+	}
+
+}
